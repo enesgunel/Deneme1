8d2413abe585309a2850c572f58b1d4f080eac19
steelbeyblade <veysioren@gmail.com>
2020-05-25 20:59:18 +0300

    silme

--- deneme/asal-sayi.py
+++ deneme/asal-sayi.py
@@ -27,20 +27,7 @@ x = 0
 asallar = [2,]
 asalMı = True
 
-while x < y:
-    x += 1
-    if x == 1:
-        continue
-    elif x > 1:
-        for i in range(2, x):
-            if (x % i == 0):
-                continue
-            elif (x % i != 0):
-                asallar.append(x)
-            x += 1
-    else:
-        print("Hata!")
-print(asallar)
+
 
 
 # deneme 12--323123
